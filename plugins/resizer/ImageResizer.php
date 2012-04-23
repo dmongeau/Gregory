0bb03e8306e2b79fcb1391b5c1eca5f943a643ef
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2012-04-23 17:38:49 -0400

    Remove debug error handling

--- plugins/resizer/ImageResizer.php
+++ plugins/resizer/ImageResizer.php
@@ -15,9 +15,9 @@ ini_set('display_errors', 0);
 error_reporting(0);
 
 
-ini_set('gd.jpeg_ignore_warning', 0);
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+//ini_set('gd.jpeg_ignore_warning', 0);
+//ini_set('display_errors', 1);
+//error_reporting(E_ALL);
 
 class ImageResizer {
 	
