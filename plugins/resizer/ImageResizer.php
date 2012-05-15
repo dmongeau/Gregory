c820fcfbb310fdeb24a14f0bd1632bb7a4bb4e91
Nicolas Roy <phatshambler@gmail.com>
2012-05-15 13:35:42 -0400

    Enabled error reporting

--- plugins/resizer/ImageResizer.php
+++ plugins/resizer/ImageResizer.php
@@ -10,14 +10,14 @@
  *
  */
  
-ini_set('gd.jpeg_ignore_warning', 1);
-ini_set('display_errors', 0);
-error_reporting(0);
+//ini_set('gd.jpeg_ignore_warning', 1);
+//ini_set('display_errors', 0);
+//error_reporting(0);
 
 
-//ini_set('gd.jpeg_ignore_warning', 0);
-//ini_set('display_errors', 1);
-//error_reporting(E_ALL);
+ini_set('gd.jpeg_ignore_warning', 0);
+ini_set('display_errors', 1);
+error_reporting(E_ALL ^ E_NOTICE);
 
 class ImageResizer {
 	
